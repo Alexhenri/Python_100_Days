d5aed704c7f3583053c390d9a26d650137caa98b
Alexandre Henrique <alexhenrii13@gmail.com>
2023-05-31 16:15:00 -0300

    Added error handling, and search button using json.

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -2,8 +2,23 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 chars = [chr(i) for i in range(33, 127)]
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search_password():
+    site = website_input.get()
+    try:
+        with open('passwords.json', 'r') as data_file:
+            data = json.load(data_file)
+            try:
+                messagebox.showinfo(f"{site}", f"Username: {data[site]['username']}\nPassword: {data[site]['password']}")
+            except KeyError:
+                messagebox.showerror("Error", f"Sorry. Not found it any password for {site}")
+    except FileNotFoundError:
+        messagebox.showerror("Error", f"Sorry. Not found it any password for {site}")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -19,9 +34,15 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    site = website_input.get()
+    site = (website_input.get()).lower()
     username = username_input.get()
     password = password_input.get()
+    new_data = {
+        site: {
+            "username": username,
+            "password": password
+        }
+    }
 
     if len(site) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showerror(title="Error", message="We not accept empty fields.")
@@ -30,8 +51,18 @@ def save_password():
     confirm = messagebox.askyesno(title="Confirm", message="Your information are correct?")
 
     if confirm:
-        with open('passwords.txt', 'a') as file:
-            file.write(f"{site} | {username}| {password}\n")
+        try:
+            with open('passwords.json', 'r') as data_file:
+                # Reading previous saved data
+                data = json.load(data_file)
+                # Updating with the new data
+                data.update(new_data)
+        except FileNotFoundError:
+            data = new_data
+        finally:
+            # Writing the new data in file
+            with open('passwords.json', 'w') as data_file:
+                json.dump(data, data_file, indent=4)  # 4 is the number os spaces of python indent
             website_input.delete(0, END)
             password_input.delete(0, END)
 
@@ -49,9 +80,12 @@ canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-website_input = Entry(width=38)
+website_input = Entry(width=21)
 website_input.focus()
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1)
+
+search_button = Button(command=search_password, text="Search", width=13)
+search_button.grid(row=1, column=2)
 
 username_label = Label(text="Username/Email:")
 username_label.grid(row=2, column=0)
