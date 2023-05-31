7e4d53134011456ae1cf620f374459a972430026
Alexandre Henrique <alexhenrii13@gmail.com>
2023-05-31 15:08:33 -0300

    Added error handling in Nato Alphabet

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,12 +1,17 @@
 import pandas
 
 file_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
-alphabet = {row.letter:row.code for (index, row) in file_csv.iterrows()}
+alphabet = {row.letter: row.code for (index, row) in file_csv.iterrows()}
 
-while True:
+
+def generate_phonetic():
     word = input("Enter a word: ")
-    if word == "exit":
-        break
-    result = [alphabet[c.upper()] for c in word]
-    print(result)
+    try:
+        result = [alphabet[c.upper()] for c in word]
+        print(result)
+    except KeyError:
+        print("Sorry, only letter in the alphabet please!")
+        generate_phonetic()
 
+
+generate_phonetic()
